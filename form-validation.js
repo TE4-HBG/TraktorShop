f0a429f4983372f3e6691df2386d5c0a072e1dd5
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2022-09-07 14:50:51 +0200

    Begun work on the JS for checkout.html

--- form-validation.js
+++ form-validation.js
@@ -18,3 +18,52 @@
       }, false)
     })
 })()
+
+
+function PrintCartItems() {
+
+  var list = '';
+  var name;
+  var img;
+  var price;
+  //code for implementing unique names and images
+  for (var i = 0; i < listToStore.length; i++) {
+      name = listToStore[i].name;
+      //console.log(name);
+      img = listToStore[i].img;
+      //console.log(img);
+      price = listToStore[i].price;
+      //console.log(price);
+
+//note för själv. imorgon gå igenom vad som ska generaras i for loopen och börja byta ut variablar som du tar ner från list to store
+
+
+      list += '<li class="list-group-item d-flex justify-content-between lh-sm">' +
+      '<div>' + 
+        '<h6 class="my-0">Product name</h6>' +
+        '<small class="text-muted">Brief description</small>' +
+      '</div>' +
+      '<span class="text-muted">$12</span>' + 
+    '</li>' +
+    '<li class="list-group-item d-flex justify-content-between lh-sm">' +
+      '<div>' +
+        '<h6 class="my-0">Second product</h6>' +
+        '<small class="text-muted">Brief description</small>' +
+      '</div>' +
+      '<span class="text-muted">$8</span>' +
+    '</li>' +
+    '<li class="list-group-item d-flex justify-content-between lh-sm">' +
+      '<div>' +
+        '<h6 class="my-0">Third item</h6>' +
+        '<small class="text-muted">Brief description</small>' +
+      '</div>' +
+      '<span class="text-muted">$5</span>' +
+    '</li>' +
+    '<li class="list-group-item d-flex justify-content-between">' +
+    '  <span>Total (USD)</span>' +
+    '  <strong>$20</strong>' +
+    '</li>';
+      
+  }
+  document.getElementById('printHere').innerHTML = list;
+}
