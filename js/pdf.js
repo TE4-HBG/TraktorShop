07b6a647ed7d858e64f02864f97919ba0f1568f7
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2022-09-08 14:04:27 +0200

    Fixed (I think) so download PDF button works

--- js/pdf.js
+++ js/pdf.js
@@ -14,7 +14,20 @@ window.onload = function () {
             html2pdf().from(invoice).set(opt).save();
         })
 }
+function makePDF() {
+    const invoice = this.document.getElementById("invoice");
+    console.log(invoice);
+    console.log(window);
+    var opt = {
+        margin: 1,
+        filename: 'myfile.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf().from(invoice).set(opt).save();
 
+}
 
 //here is out copy pasted code for the print functions
 
