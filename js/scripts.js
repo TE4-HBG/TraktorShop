ba951bad1e4f2632018eb307c905c688d2bdadbe
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-09-08 09:32:49 +0200

    Commented js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+//the CartItem class conveniently contains and handles information about our tractors. These variables will later be saved to the local storage to be re-displayed in the cart.
 class CartItem {
     constructor(name, img, price) {
         this.name = name;
@@ -13,6 +14,8 @@ class CartItem {
         this.price = price;
     }
 }
+
+//Function to clear the cart completely.
 
 function ClearCartList() {
     listToStore = [];
@@ -23,6 +26,8 @@ function ClearCartList() {
     CalcTotalPrice();
 }
 
+//Function used to load the necessary information when the cart.html page is loaded.
+
 function InitCart()
 {
     GetFromLocalStorage();
@@ -31,7 +36,9 @@ function InitCart()
     
 }
 
-function reCreateList(arr) {
+//The ReCreateList function is used when receiving information from the local storage. As the local storage only accepts strings, we have to first turn our cart items into strings, then turn them into CartItems again when we retrieve the information.
+
+function ReCreateList(arr) {
 
     let newArr = [];
     let i = 0;
@@ -42,10 +49,13 @@ function reCreateList(arr) {
     return newArr;
 }
 
+//Variables used in the process of setting and getting information from and to the local storage.s
 let listToStore = [];
 let splitArr = [];
 let identifier = ',';
 
+
+//The RetrieveItem function is used when a user clicks a "Add to cart" button on a html page. It retrieves the information of the specific CartItem linked with the button and prepares it to be set in the local storage.
 function RetrieveItem(button, img, price) {    
     listToStore.push(new CartItem(button.getAttribute("name"), img, price));
     document.getElementById('itemNumberDisplay').innerHTML = listToStore.length;
@@ -53,9 +63,13 @@ function RetrieveItem(button, img, price) {
     SetToLocalStorage();
 }
 
+//The AddIdentifier function puts every cartItem into a separate list and adds commas. These commas allow us to store the information as a string and later retrieve it as a CartItem.
 function AddIdentifier() {
+
     //Genom att sätta splitArr till en blank array, lyckas vi undvika problemet där splitArr får till sig extra element efter den hämtas av "localstorage.GetItem".
     //Utan att "tömma" splitArr läggs extra objekt in i arrayen.
+    //By setting splitArr to an empty array we avoid the problem where splitArr adds extra elements to itself when "localStorage.GetItem" is run.
+    //The array displays the correct amount of items after it is "emptied".
     splitArr.length = 0;
     //console.log(splitArr);
 
@@ -68,7 +82,7 @@ function AddIdentifier() {
     //console.log(splitArr);
 }
 
-//Puts information into the local web storage so that information on our site can be saved through 
+//Puts information into the local web storage so that information on our site can be saved through page refreshes
 function SetToLocalStorage() {
 
     localStorage.setItem('item', splitArr);
@@ -83,7 +97,7 @@ function GetFromLocalStorage() {
     if (str !== null) {
         //console.log(str);
         let tmp = str.split(identifier);
-        listToStore = reCreateList(tmp.filter(n => n));
+        listToStore = ReCreateList(tmp.filter(n => n));
         //console.log(listToStore);
     }
     document.getElementById('itemNumberDisplay').innerHTML = listToStore.length;
@@ -97,7 +111,8 @@ function RepopulateList()
     PrintCartItems();
     CalcTotalPrice();
 }
-//PrintCardtItems prints every single item in the cart by first writing their HTML to a variable, then through a for-loop, changing certain values such as "name" to be personalized for every item.
+
+//PrintCardtItems draws every single item in the cart on the page by first setting the item's HTML structure to a variable, then through a for-loop changing certain values such as "name" to be personalized for every item.
 function PrintCartItems() {
 
     var list = '';
@@ -137,6 +152,7 @@ function PrintCartItems() {
     document.getElementById('cart').innerHTML = list;
 }
 
+//CalcTotalPrice calculates the total price of items inside the cart and displays it.
 function CalcTotalPrice()
 {
     let intPrice = 0;
